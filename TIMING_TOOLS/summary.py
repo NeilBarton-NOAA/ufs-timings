9630f6daae2ff2e8584b8d41452eac4eae8d811a
NeilBarton-NOAA <neil.barton@noaa.gov>
2023-07-03 18:46:59 +0000

    move pretty table to pandas

--- TIMING_TOOLS/summary.py
+++ TIMING_TOOLS/summary.py
@@ -4,7 +4,7 @@ __all__ = ['write']
 def write(df, ARGS):
     import pandas as pd
     import numpy as np
-    from prettytable import PrettyTable as pt
+    import os
     # create pandas from summaries
     pd.options.display.max_rows = None
     pd.options.display.max_columns = 99
@@ -106,19 +106,19 @@ def write(df, ARGS):
     HEAD_PRINT.append('UNUSED_PROCS')
     HEAD_PRINT.append('COREHOURSpDAY')
     # remove items that are the same 
+    SUM = []
     if df.shape[0] > 1:
-        SUM = pt(['SHARED:', ' '], border = False, padding_width = 2)
+        SUM.append(['SHARED:', '  '])
     else:
-        SUM = pt(['INFO:', ''], border = False, padding_width = 2)
-    SUM.align = 'l'
+        HEADER = ['INFO:', ' ']
     LOOP = HEAD_PRINT.copy()
     for H in LOOP:
         if (df[H] == df[H][1]).all():
-            SUM.add_row([H+':', str(df[H][1])])
+            SUM.append([H+':', str(df[H][1])])
             HEAD_PRINT.remove(H)
     
     if PRINT_ATMIO:
-        SUM.add(['WARNING:', 'ATMIOsec_max is close to or greater than ATMsec_max'])
+        SUM.append(['WARNING:', 'ATMIOsec_max is close to or greater than ATMsec_max'])
 
     if ARGS.SORTBY not in HEAD_PRINT:
         HEAD_PRINT.append(ARGS.SORTBY)
@@ -126,19 +126,30 @@ def write(df, ARGS):
     df = df.sort_values('CONFIG')
     df = df.sort_values('TAU')
     df = df.sort_values(ARGS.SORTBY)
-    fw = 'esmf_summary.txt'
+    
+    # print to screen
+    df_sum = pd.DataFrame(SUM)
     print('\n')
-    print(SUM)
+    print(df_sum.to_string(index=False, header=False)) 
     print('\n')
     if df.shape[0] > 1:
         print(df[HEAD_PRINT])
-        df[HEAD_PRINT].to_string(fw) 
-    f = open(fw,'a')
-    f.write('\n\n')
-    f.write(SUM.get_string())
-    f.write('\n\n') 
+    # write to file
+    fw = 'esmf_summary.txt'
+    f = open(fw,'w')
+    f.write('RUN DIRECTORIES:\n')
     for i in range(df.shape[0]):
         f.write(str(i+1) + ' ' + df['NAME'][i+1] + '\n')
     f.close()
-
+    df_sum.to_string('temp1.txt', index=False, header=False)
+    if df.shape[0] > 1:
+        df[HEAD_PRINT].to_string('temp2.txt') 
+    with open(fw,'a') as outfile:
+        for f in ['temp1.txt', 'temp2.txt']:
+            with open(f) as infile:
+                outfile.write('\n')
+                outfile.write(infile.read())
+                outfile.write('\n')
+            os.remove(f)
+   
 
